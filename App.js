07d29859b52c597a77232926f682d5059a8f1a4a
snehasushil10 <143604446+snehasushil10@users.noreply.github.com>
2024-01-21 11:32:02 +0530

    props using loop

---
+++ App.js
@@ -0,0 +1,51 @@
+import ExpenseItem from "./components/ExpenseItem";
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: "Bangalore",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      location: "Delhi",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: "Kerala",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: "Tamil Nadu",
+    },
+  ];
+  const expenseItems = [];
+
+  for (let i = 0; i < expenses.length; i++) {
+    const expense = expenses[i];
+    expenseItems.push(
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        location={expense.location}
+      />
+    );
+  }
+
+  return <div>{expenseItems}</div>;
+}
+export default App;
+
--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,26 +1,24 @@
-import './ExpenseItem.css';
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-  const locationofExpenditure ="bangalore";
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
+  //const expenseDate = new Date(2021, 2, 28);
+  //const expenseTitle = "Car Insurance";
+  //const expenseAmount = 294.67;
+  //const locationofExpenditure = "bangalore";
   return (
     //<div>
-    // <h2>Expense items</h2>
-    //  <div>Food Rs 10</div>
-    //   <div>Petrol Rs 100</div>
-    // <div>Movies Rs 200</div>
+    //<h2>Expense items</h2>
+    //<div>Food Rs 10</div>
+    //<div>Petrol Rs 100</div>
+    //<div>Movies Rs 200</div>
     //</div>
-
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
-        <div className='expense__location'>{locationofExpenditure}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense__location">{props.location}</div>
       </div>
     </div>
   );
 }
-
 export default ExpenseItem;
