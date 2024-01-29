e8fc934c7dbd2ebfad8c1ee21b262102510749f9
snehasushil10 <143604446+snehasushil10@users.noreply.github.com>
2024-01-29 13:56:11 +0530

    Adding Event Listeners made easy(task 1)

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -2,12 +2,19 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
-const ExpenseItem=(props)=> {
+const ExpenseItem = (props) => {
   //const expenseDate = new Date(2021, 2, 28);
   //const expenseTitle = "Car Insurance";
   //const expenseAmount = 294.67;
   //const locationofExpenditure = "bangalore";
- 
+  const clickHandler = () => {
+    console.log("Clicked!!!");
+  };
+  const deleteExpenseHandler = () => {
+    const expenseitem = document.querySelector(".expense-item");
+    expenseitem.remove();
+  };
+
   return (
     //<div>
     //<h2>Expense items</h2>
@@ -16,10 +23,15 @@ const ExpenseItem=(props)=> {
     //<div>Movies Rs 200</div>
     //</div>
     <Card className="expense-item">
-     <ExpenseDate date={props.date}/>
-     <ExpenseDetails  title = {props.title}  amount ={props.amount} location = {props.location}/>
-     
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        title={props.title}
+        amount={props.amount}
+        location={props.location}
+      />
+      <button onClick={clickHandler}>Change title</button>
+      <button onClick={deleteExpenseHandler}>Delete ExpenseItem</button>
     </Card>
   );
-}
+};
 export default ExpenseItem;
